3958890a0dd2f053fd3333666d6ec39a5d5f66df
egemen@Orange <egemensert@gmail.com>
2018-02-05 01:26:11 +0300

    bug fix

--- dqn.py
+++ dqn.py
@@ -1,186 +1,101 @@
 import gym
-import math
-import random
 import numpy as np
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Input
+from keras.layers.merge import Add, Multiply
+from keras.optimizers import Adam
+
+from collections import deque
+import random
+
 import matplotlib
 import matplotlib.pyplot as plt
-from collections import namedtuple
-from itertools import count
-from copy import deepcopy
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.autograd import Variable
-import torchvision.transforms as T
+class DQNAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.model = self._build_model()
 
-env = gym.make('CartPole-v0')
-plt.ion()
+    def _build_model(self):
+        model = Sequential()
+        model.add(Dense(24, input_dim = self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
 
-use_cuda = torch.cuda.is_available()
-FloatTensor = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
-LongTensor = torch.cuda.LongTensor if use_cuda else torch.LongTensor
-ByteTensor = torch.cuda.ByteTensor if use_cuda else torch.ByteTensor
-Tensor = FloatTensor
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
 
-Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+    def act(self, state):
+        if np.random.randn() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])
 
-class ReplayMemory(object):
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.memory = []
-        self.position = 0
+    def replay(self, batch_size):
+        if len(self.memory) < batch_size:
+            return
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = reward + self.gamma * \
+                    np.amax(self.model.predict(next_state)[0])
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+        else:
+            self.epsilon = self.epsilon_min
 
-    def push(self, *args):
-        if len(self.memory) < self.capacity:
-            self.memory.append(None)
-        self.memory[self.position] = Transition(*args)
-        self.position = (self.position + 1) % self.capacity
-
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
-        return len(self.memory)
-
-class DQN(nn.Module):
-    def __init__(self, input_size, output_size):
-        super(DQN, self).__init__()
-        self.nn1 = nn.Linear(input_size, 128)
-        self.d = nn.Dropout(0.5)
-        self.nn2 = nn.Linear(128, 128)
-        self.nn3 = nn.Linear(128, output_size)
-
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                n = 128**2
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, nn.BatchNorm1d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-
-    def forward(self, x):
-        x = F.relu(self.d(self.nn1(x)))
-        x = F.relu(self.d(self.nn2(x)))
-        return self.nn3(x)
-
-BATCH_SIZE = 128
-GAMMA = 0.999
-EPS_START = 0.9
-EPS_END = 0.05
-EPS_DECAY = 200
-
-model = DQN(4, 2)
-
-if use_cuda:
-    model.cuda()
-
-optimizer = optim.Adam(model.parameters(), lr=1e-4)
-memory = ReplayMemory(10000)
-
-steps_done = 0
-
-def select_action(state):
-    global steps_done
-    sample = random.random()
-    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
-        math.exp(-1. * steps_done / EPS_DECAY)
-    steps_done += 1
-    if sample > eps_threshold:
-        model.eval()
-        return model(Variable(state, volatile=True).type(FloatTensor)).data.max(1)[1].view(1, 1)
-    else:
-        return LongTensor([[random.randrange(2)]])
-
-episode_durations = []
-
-def plot_durations():
+def plot_durations(durations_t, means):
     plt.figure(2)
     plt.clf()
-    durations_t = torch.FloatTensor(episode_durations)
     plt.title('Training...')
     plt.xlabel('Episode')
     plt.ylabel('Duration')
-    plt.plot(durations_t.numpy())
+    plt.plot(durations_t, alpha=0.3)
     # Take 100 episode averages and plot them too
     if len(durations_t) >= 100:
-        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
-        means = torch.cat((torch.zeros(99), means))
-        plt.plot(means.numpy())
+        means.append(np.array(durations_t[-100:]).mean())
+        plt.plot(means)
 
     plt.pause(0.001)  # pause a bit so that plots are updated
+    plt.savefig('figfig')
 
-last_sync = 0
+if __name__ == "__main__":
+    env = gym.make('CartPole-v0')
+    agent = DQNAgent(4, 2)
 
-def optimize_model():
-    global last_sync
-    if len(memory) < BATCH_SIZE:
-        return
+    episodes = 5000
+    means = [0 for i in range(99)]
+    durations = []
+    for e in range(episodes):
+        state = env.reset()
+        state = state.reshape((1, 4))
 
-    model.train()
-    transitions = memory.sample(BATCH_SIZE)
-    batch = Transition(*zip(*transitions))
+        for time_t in range(500):
+            action = agent.act(state)
 
-    non_final_mask = ByteTensor(tuple(map(lambda s: s is not None,
-                                        batch.next_state)))
+            next_state, reward, done, _ = env.step(action)
+            next_state = next_state.reshape(1, 4)
 
-    non_final_next_states = Variable(torch.cat([s for s in batch.next_state
-                                                if s is not None]),
-                                     volatile=True)
+            agent.remember(state, action, reward, next_state, done)
 
-    state_batch = Variable(torch.cat(batch.state))
-    action_batch = Variable(torch.cat(batch.action))
-    reward_batch = Variable(torch.cat(batch.reward))
+            state = next_state
 
-    # Q(s_t, a_t)
-    state_action_values = model(state_batch).gather(1, action_batch)
-    # V(s_t+1)
-    next_state_values = Variable(torch.zeros(BATCH_SIZE).type(Tensor))
-    next_state_values[non_final_mask] = model(non_final_next_states).max(1)[0]
-    next_state_values.volatile = False
+            if done:
+                durations.append(time_t)
+                plot_durations(durations, means)
+                break
 
-    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
-
-    loss = F.smooth_l1_loss(state_action_values, expected_state_action_values)
-
-    optimizer.zero_grad()
-    loss.backward()
-    for param in model.parameters():
-        param.grad.data.clamp_(-1, 1)
-    optimizer.step()
-    return loss.data[0]
-
-num_episodes = 10000
-losses = 0.
-it = 0.
-for i_episode in range(num_episodes):
-    state = env.reset()
-    state = torch.from_numpy(state).unsqueeze(0).type(Tensor)
-
-    for t in count():
-
-        action = select_action(state)
-        next_state, reward, done, _ = env.step(action[0, 0])
-        reward = Tensor([reward])
-
-        next_state = torch.from_numpy(next_state).unsqueeze(0).type(Tensor)
-        memory.push(state, action, next_state, reward)
-
-        state = next_state
-        loss = optimize_model()
-        if loss:
-            losses += loss
-            it +=1
-        if done:
-            episode_durations.append(t+1)
-            plot_durations()
-            if it:
-                print "Loss: %.4f" % (losses/it)
-            break
-
-print("Complete")
-env.render(close=True)
-env.close()
-plt.ioff()
-plt.show()
+        agent.replay(32)
